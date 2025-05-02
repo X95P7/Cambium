f4dc0aae0e686c4ffcf0bc735df450410bd89c8e
X95P7 <xanderede@gmail.com>
2025-05-02 15:14:41 -0500

    Launch headlessmc From .yml file

--- headlessmc/entrypoint.sh
+++ headlessmc/entrypoint.sh
@@ -1,22 +1,39 @@
-#!/bin/sh
+#!/usr/bin/expect -f
 
-echo "Waiting for Minecraft server to start..."
-until nc -z -v -w30 mc-forge 25565; do
-  echo "Waiting for mc-forge on port 25565..."
-  sleep 5
-done
+# Never timeout
+set timeout -1
 
-echo "Minecraft server detected. Launching HeadlessMC setup..."
+# 1) Wait for the Minecraft server to be up
+puts "Waiting for mc-forge to start..."
+spawn sh -c {
+  until nc -z -v -w30 mc-forge 25565; do
+    echo "Waiting for mc-forge on port 25565..."
+    sleep 5
+  done
+}
+expect eof
 
-# Ensure the base game and Forge are downloaded before launching
-java -jar headlessmc-launcher-${HMC_VERSION}.jar --command download ${MC_VERSION}
-java -jar headlessmc-launcher-${HMC_VERSION}.jar --command forge ${MC_VERSION} --java $JAVA_HOME/bin/java
+# 2) Login to HeadlessMC
+puts "Logging into HeadlessMC..."
+spawn xvfb-run -a java -Dhmc.check.xvfb=true \
+  -jar headlessmc-launcher-${env(HMC_VERSION)}.jar \
+  --command login ${env(EMAIL)} ${env(PASSWORD)}
+expect eof
 
-# Login
-xvfb-run java -Dhmc.check.xvfb=true -jar headlessmc-launcher-${HMC_VERSION}.jar --command login ${EMAIL} ${PASSWORD}
+# 3) Enter the interactive HeadlessMC prompt
+puts "Entering HeadlessMC prompt..."
+spawn xvfb-run -a java -Dhmc.check.xvfb=true \
+  -jar headlessmc-launcher-${env(HMC_VERSION)}.jar \
+  --command launch forge:${env(MC_VERSION)}
 
-# Launch the game
-xvfb-run java -Dhmc.check.xvfb=true -jar headlessmc-launcher-${HMC_VERSION}.jar --command launch forge:1.8.9 $HEADLESSMC_COMMAND
+# 4) Send launch and connect commands
+# 1) Wait for that unique “Sound engine started” line
+expect {
+  -re {Client thread/INFO\]: Created: 1024x512 textures-atlas} {
+  send "connect $env(SERVER)\r"
+    send "say Hello, world!\r"
+    # once we see it, fall through to next step
+  }
+}
 
-# Finally, connect to the server
-xvfb-run java -Dhmc.check.xvfb=true -jar headlessmc-launcher-${HMC_VERSION}.jar --command connect ${SERVER}
+expect eof
